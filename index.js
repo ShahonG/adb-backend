107638d4906a6767be2884847aae903c8a6f601c
SarunWu <96522041+SarunWu@users.noreply.github.com>
2023-01-07 16:12:05 +0800

    reorganize index.js

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
-const express = require('express');
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.use(express.json())
-app.use(express.urlencoded({ extended:true }))
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // ALL
-app.use('/get_suppliers', require('./routes/get_suppliers.js'))
-app.use('/get_orders', require('./routes/get_orders.js'))
-app.use('/get_products', require('./routes/get_products.js'))
+app.use("/get_suppliers", require("./routes/get_suppliers.js"));
+app.use("/get_orders", require("./routes/get_orders.js"));
+app.use("/get_products", require("./routes/get_products.js"));
 
 // Return
-app.use('/get_return_suppliers', require('./routes/get_return_suppliers.js'))
-app.use('/get_return_orders', require('./routes/get_return_orders.js'))
-app.use('/get_return_products', require('./routes/get_return_products.js'))
+app.use("/get_return_suppliers", require("./routes/get_return_suppliers.js"));
+app.use("/get_return_orders", require("./routes/get_return_orders.js"));
+app.use("/get_return_products", require("./routes/get_return_products.js"));
 
 // Cancel
-app.use('/get_cancel_suppliers', require('./routes/get_cancel_suppliers.js'))
-app.use('/get_cancel_orders', require('./routes/get_cancel_orders.js'))
-app.use('/get_cancel_products', require('./routes/get_cancel_products.js'))
+app.use("/get_cancel_suppliers", require("./routes/get_cancel_suppliers.js"));
+app.use("/get_cancel_orders", require("./routes/get_cancel_orders.js"));
+app.use("/get_cancel_products", require("./routes/get_cancel_products.js"));
 
 // Other
-app.use('/get_cities', require('./routes/get_cities.js'))
-app.use('/get_district', require('./routes/get_district.js'))
-app.use('/get_orders_by_suppliers', require('./routes/get_orders_by_supplier.js'))
-app.use('/get_suppliers_list', require('./routes/get_suppliers_list.js'))
-
+app.use("/get_cities", require("./routes/get_cities.js"));
+app.use("/get_district", require("./routes/get_district.js"));
+app.use(
+  "/get_orders_by_suppliers",
+  require("./routes/get_orders_by_supplier.js")
+);
+app.use("/get_suppliers_list", require("./routes/get_suppliers_list.js"));
 
 app.listen(port, () => {
-    console.log(`Success, view the app at http://localhost:${port}`)
-})
+  console.log(`Success, view the app at http://localhost:${port}`);
+});
